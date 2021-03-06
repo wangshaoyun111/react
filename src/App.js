02a17833bdd8b069ce95bec656343621187a2e59
wsy <18611031643.163.com>
2021-03-06 16:35:21 +0800

    安装andt-mobile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+// 按需导入
+import { Button } from 'antd-mobile';
 function App() {
   return (
     <div className="App">
-      我是跟组件
+      <p>我是跟组件</p>
+      <Button>Start</Button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+// 引入antd-mobile 组件库
+import 'antd-mobile/dist/antd-mobile.css'
+
 import './index.css'
 import App from './App'
 
