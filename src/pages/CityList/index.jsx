390fdcfd9d6f369be91ef8a4ec136d7bcabdd8f2
wsy <18611031643.163.com>
2021-03-07 14:44:24 +0800

    完成头部城市导航

--- src/pages/CityList/index.jsx
+++ src/pages/CityList/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 
+import { NavBar } from 'antd-mobile'
+
+import './index.scss'
 export default class CityList extends React.Component {
     render() {
         return (
-            <div>
-                <h4>我是CityList组件</h4>
+            // 顶部导航
+            <div className='ciyilist-container'>
+                <NavBar
+                    className='navBar'
+                    mode="light"
+                    icon={<i className='iconfont icon-back'></i>}
+                    onLeftClick={() => this.props.history.go(-1)}>
+                    城市选择
+                </NavBar>
             </div>
         )
     }
