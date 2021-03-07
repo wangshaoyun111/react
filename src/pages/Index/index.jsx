d17d4ca0d3e22f6e40154e8935648bd89d82415b
wsy <18611031643.163.com>
2021-03-07 11:01:17 +0800

    完成租房小组

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 // 导图axios
 import axios from 'axios'
 // 导入轮播图组件,flex组件
-import { Carousel, Flex } from 'antd-mobile';
+import { Carousel, Flex,Grid  } from 'antd-mobile';
 
 // 导入样式文件
-import './index.css'
+import './index.scss'
 // 导入图片
 import Nav1 from '../../assets/images/nav-1.png'
 import Nav2 from '../../assets/images/nav-2.png'
@@ -15,42 +15,45 @@ import Nav4 from '../../assets/images/nav-4.png'
 // 导航数据
 const navs = [
     {
-        id: 1,
-        img: Nav1,
-        title: '整租',
-        path: '/home/list'
-      },
-      {
-        id: 2,
-        img: Nav2,
-        title: '合租',
-        path: '/home/list'
-      },
-      {
-        id: 3,
-        img: Nav3,
-        title: '地图找房',
-        path: '/map'
-      },
-      {
-        id: 4,
-        img: Nav4,
-        title: '去出租',
-        path: '/rent/add'
-      }
-    ]    
+      id: 1,
+      img: Nav1,
+      title: '整租',
+      path: '/home/list'
+    },
+    {
+      id: 2,
+      img: Nav2,
+      title: '合租',
+      path: '/home/list'
+    },
+    {
+      id: 3,
+      img: Nav3,
+      title: '地图找房',
+      path: '/map'
+    },
+    {
+      id: 4,
+      img: Nav4,
+      title: '去出租',
+      path: '/rent/add'
+    }
+]    
 export default class Index extends React.Component {
     state = {
         swipers: [], // 数据
-        IndexFlag: false // 数据完全返回在渲染轮播图，解决bug
+        IndexFlag: false, // 数据完全返回在渲染轮播图，解决bug
+        groups:[] // 租房小组数据
     }
 
     componentDidMount() {
-        // 调用获取数据方法
+        // 调用轮播获取数据方法
         this.getSwipers()
+        // 租房信息数据
+        this.getGroup()
     }
 
-    // 获取数据的方法
+    // 获取轮播数据的方法
     async getSwipers() {
         const {data: res} = await axios.get('http://api-haoke-web.itheima.net/home/swiper')
         console.log(res)
@@ -60,7 +63,15 @@ export default class Index extends React.Component {
             IndexFlag: true
         })
     }
-
+    // 获取租房小组数据
+    async getGroup() {
+        const { data: res } = await axios.get('http://api-haoke-web.itheima.net/home/groups')
+        console.log(res);
+        if (res.status !== 200) return
+        this.setState({
+            groups: res.body
+        })
+    }
     // 渲染轮播图结构方法
     renderSwipwe() {
         return this.state.swipers.map(item => (
@@ -105,6 +116,30 @@ export default class Index extends React.Component {
                 <Flex className='nav'>
                     { this.renderNavs() }
                 </Flex>
+
+                {/* 租房小组 */}
+                <div className="group">
+                <h3 className="group-title">
+                    租房小组 <span className="more">更多</span>
+                </h3>
+
+                {/* 宫格组件 */}
+                <Grid
+                    data={this.state.groups}
+                    columnNum={2}
+                    square={false}
+                    hasLine={false}
+                    renderItem={item => (
+                    <Flex className="group-item" justify="around" key={item.id}>
+                        <div className="desc">
+                        <p className="title">{item.title}</p>
+                        <span className="info">{item.desc}</span>
+                        </div>
+                        <img src={'http://api-haoke-web.itheima.net' + item.imgSrc} alt="" />
+                    </Flex>
+                    )}
+                />
+                </div>
             </div>
         )
     }
