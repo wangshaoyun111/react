da4726a9868ed8411d5036afb872b45527b5dc2e
wsy <18611031643.163.com>
2021-03-16 20:08:03 +0800

    渲染所有区的房源覆盖物,请求数据，遍历数据，创建覆盖物

--- src/pages/Map/index.jsx
+++ src/pages/Map/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavHeader from '../../components/NavHeader/index'
 import styles from './index.module.css'
+import axios from 'axios'
 // 覆盖物样式
 const labelStyle = {
     cursor: 'pointer',
@@ -24,35 +25,51 @@ export default class Profile extends React.Component {
         const myGeo = new window.BMap.Geocoder()
 
         // 根据地址定位当前地图展示信息
-        myGeo.getPoint(label, function(point) {
+        myGeo.getPoint(label, async function(point) {
             if (point) {
                 // centerAndZoom 方法 调整地图的缩放级别
                 map.centerAndZoom(point, 11)
                 map.addControl(new window.BMap.ScaleControl())
                 map.addControl(new window.BMap.NavigationControl())
-                const opts = {
-                    position: point,    // 指定文本标注所在的地理位置
-                    offset: new window.BMap.Size(20, 20)    //设置文本偏移量
-                }
-                // 创建label 方法，绘制文本覆盖物,
-                // 第一个参数是需要添加文本
-                // 第二个参数为文本覆盖物的定位
-                const label = new window.BMap.Label("文本信息~", opts)
-                label.setContent(`
-                    <div class="${styles.bubble}">
-                        <p class="${styles.name}">航头镇</p>
-                        <p>100套</p>
-                    </div>
-                `)
-                // 设置样式
-                label.setStyle(labelStyle)
 
-                // 给房源覆盖物绑定点击事件
-                label.addEventListener('click', () => {
-                    console.log(111);
+                // 调用接口获取房源数据
+                const { data: res } = await axios.get(`https://api-haoke-web.itheima.net/area/map?id=${value}`)
+                if (res.status !== 200) return
+                res.body.forEach(item => {
+                    const {
+                        coord: { latitude, longitude },
+                        label: areaName,
+                        count,
+                        value
+                    } = item
+                    // 根据获取的精度维度 通过方法，生产地理位置
+                    const areaPoint = new window.BMap.Point(longitude, latitude)
+                    console.log(item);
+                    const opts = {
+                        position: areaPoint,    // 指定文本标注所在的地理位置
+                        offset: new window.BMap.Size(20, 20)    //设置文本偏移量
+                    }
+                    // 创建label 方法，绘制文本覆盖物,
+                    // 第一个参数是需要添加文本
+                    // 第二个参数为文本覆盖物的定位
+                    const label = new window.BMap.Label("", opts)
+                    label.id = value
+                    label.setContent(`
+                        <div class="${styles.bubble}">
+                            <p class="${styles.name}">${areaName}</p>
+                            <p>${count}</p>
+                        </div>
+                    `)
+                    // 设置样式
+                    label.setStyle(labelStyle)
+
+                    // 给房源覆盖物绑定点击事件
+                    label.addEventListener('click', () => {
+                        console.log(label.id);
+                    })
+                    // 将覆盖物添加到地图中
+                    map.addOverlay(label)
                 })
-                // 将覆盖物添加到地图中
-                map.addOverlay(label)
             }
         }, label)
     }
