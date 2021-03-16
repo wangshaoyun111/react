2f2e889302853ace5f00205579182d74ffcd0b71
wsy <18611031643.163.com>
2021-03-16 14:13:21 +0800

    根据定位展示当前城市,在地图中展示该城市，并添加比例尺和平移缩放控件

--- src/pages/Map/index.jsx
+++ src/pages/Map/index.jsx
@@ -7,20 +7,32 @@ export default class Profile extends React.Component {
         // 初始化地图实例
         const map = new window.BMap.Map("container")
 
-        // 设置中心坐标点
-        const point = new window.BMap.Point(116.404, 39.915)
+        // 获取到当前城市定位信息
+        const { label, value } = JSON.parse(localStorage.getItem('hkzf_city'))
 
-        // 初始化地图
-        map.centerAndZoom(point, 15)
+        // 调用百度地图解析器,生成解析器实例
+        const myGeo = new window.BMap.Geocoder()
+
+        // 根据地址定位当前地图展示信息
+        myGeo.getPoint(label, function(point) {
+            if (point) {
+                // centerAndZoom 方法 调整地图的缩放级别
+                map.centerAndZoom(point, 11)
+
+                map.addControl(new window.BMap.ScaleControl())
+                map.addControl(new window.BMap.NavigationControl())
+            }
+        }, label)
     }
     render() {
         return (
             <div className={styles.map_container}>
-                {/* 顶部导航栏 */}
+                {/* 顶部导航栏功能 */}
                 <div className={styles.my_navbar}>
                     <NavHeader onLeftClick={() => this.props.history.go(-1)}>地图找房</NavHeader>
                 </div>
-                {/* 地图容器 */}
+
+                {/* 地图容器元素 */}
                 <div id="container" className={styles.container}></div>
             </div>
         )
