15ddcd1069d2cb64dad5c924e09c4b92dd02fc23
wsy <18611031643.163.com>
2021-03-25 09:11:34 +0800

    封装条件筛选房源

---
+++ src/components/FilterFooter/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import { Flex } from 'antd-mobile'
+import PropTypes from 'prop-types'
+
+import styles from './index.module.css'
+
+function FilterFooter({
+  cancelText = '取消',
+  okText = '确定',
+  onCancel,
+  onOk,
+  className
+}) {
+  return (
+    <Flex className={[styles.root, className || ''].join(' ')}>
+      {/* 取消按钮 */}
+      <span
+        className={[styles.btn, styles.cancel].join(' ')}
+        onClick={onCancel}
+      >
+        {cancelText}
+      </span>
+
+      {/* 确定按钮 */}
+      <span className={[styles.btn, styles.ok].join(' ')} onClick={onOk}>
+        {okText}
+      </span>
+    </Flex>
+  )
+}
+
+// props校验
+FilterFooter.propTypes = {
+  cancelText: PropTypes.string,
+  okText: PropTypes.string,
+  onCancel: PropTypes.func,
+  onOk: PropTypes.func,
+  className: PropTypes.string
+}
+
+export default FilterFooter
--- src/pages/HouseList/index.jsx
+++ src/pages/HouseList/index.jsx
@@ -6,6 +6,9 @@ import { Flex } from 'antd-mobile'
 // 导入封装的顶部搜索栏组件
 import SearchHeader from '../../components/SearchHeader/index.jsx'
 
+// 导入组件
+
+import Filter from './components/Filter/index'
 import styles from './index.module.css'
 // 获取本地存储的城市信息
 const { label } = JSON.parse(localStorage.getItem('hkzf_city'))
@@ -18,6 +21,9 @@ export default class HouseList extends React.Component {
                     <i className="iconfont icon-back" onClick={() => this.props.history.go(-1)} />
                     <SearchHeader className={styles.searchHeader} currentCityName={label}></SearchHeader>
                 </Flex>
+
+                {/* 调用条件筛选栏 */}
+                <Filter></Filter>
             </div>
         )
     }
