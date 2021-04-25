f5145584cddc296bf5a35f93c2f01892af9ea2a8
wsy <18611031643.163.com>
2021-04-25 15:29:28 +0800

    完成搜索关键字，用防抖来提高性能

--- src/pages/Rent/Search/index.js
+++ src/pages/Rent/Search/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import { SearchBar } from 'antd-mobile'
 
+import { API } from '../../../utils/api.js'
+
 import { getCity } from '../../../utils/city.js'
 
 import styles from './index.module.css'
@@ -26,7 +28,32 @@ export default class Search extends Component {
       </li>
     ))
   }
-
+  // 获取搜索关键字
+  handleSearchTxt = async (value) => {
+    this.setState({
+      searchTxt: value
+    })
+    // 判断输入框是否为空
+    if (!value.trim().length === 0) {
+      return this.setState({
+        searchTxt: '',
+        tipsList: []
+      })
+    }
+    clearTimeout(this.timerId)
+    this.timerId = setTimeout(async () => {
+      const { data: res } = await API.get('/area/community', {
+        params: {
+          name: value,
+          id: this.cityId
+        }
+      })
+      if (res.status !== 200) return
+      this.setState({
+        tipsList: res.body
+      })
+    }, 500)
+  }
   render() {
     const { history } = this.props
     const { searchTxt } = this.state
@@ -39,6 +66,7 @@ export default class Search extends Component {
           value={searchTxt}
           showCancelButton={true}
           onCancel={() => history.replace('/rent/add')}
+          onChange={this.handleSearchTxt}
         />
 
         {/* 搜索提示列表 */}
